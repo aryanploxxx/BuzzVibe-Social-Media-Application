33898f5ee1f0d5d441a5500b2610db21ebc6218a
aryanploxxx <aryangupta8291@gmail.com>
2024-06-20 20:22:35 +0530

    Feature: Protected Route

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,18 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home/Home.js'
 import Login from './pages/Login/Login.js'
 import Signup from './pages/Login/Signup.js'
+import ProtectedRoute from './pages/ProtectedRoute.js';
+import PageLoading from './pages/PageLoading.js';
 
 function App() {
   return (
     <div className="App">
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<Home/>} />
+            <Route path='/' element={<ProtectedRoute><Home/></ProtectedRoute>} />
             <Route path='/login' element={<Login/>} />
             <Route path='/signup' element={<Signup/>} />
+            <Route path='/page-loading' element={<PageLoading/>} />
           </Routes>
         </BrowserRouter>
 
