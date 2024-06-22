165916dc760d659fb3b4b881d52bb44d9850de90
aryanploxxx <aryangupta8291@gmail.com>
2024-06-22 05:49:04 +0530

    Feature: Add Widgets

--- client/src/pages/Widgets/Widgets.js
+++ client/src/pages/Widgets/Widgets.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import './Widgets.css'
+import React from "react";
+import "./Widgets.css";
+import { TwitterTimelineEmbed, TwitterTweetEmbed } from "react-twitter-embed";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Widgets = () => {
   return (
-    <div>Widgets</div>
-  )
+    <div className="widgets">
+      <div className="widgets_input">
+        <SearchIcon className="widgets_searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+
+      <div className="widgets_widgetContainer">
+        <h2>What's happening</h2>
+
+        <TwitterTweetEmbed tweetId={"1800275692107465059"} />
+
+        <TwitterTimelineEmbed sourceType="profile" screenName="MrBeast" options={{ height: 400 }} />
+
+
+      </div>
+    </div>
+  );
 }
 
-export default Widgets
+export default Widgets;
